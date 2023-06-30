df42acc4f4822f89071de5107288a3c80b3bed3f
HmurihMonakh <108191746+HmurihMonakh@users.noreply.github.com>
2023-06-30 17:48:10 +0300

    Update and rename task40.py to task40,42.py

--- DZ1_P/task40,42.py
+++ DZ1_P/task40,42.py
@@ -12,6 +12,10 @@ df[
     (df['population'] < 500)
 ].median_house_value.mean()
 
+
+# Задача 42: Узнать какая максимальная households в зоне минимального значения population.
+
+
 min_population = df.population.min()
 df[
     df['population'] == min_population
